b84f7d38d19842a9e78dce6a37175622bf55ad1a
Harikrishna C <harikrishna.cvv230149@cvv.ac.in>
2024-06-24 19:49:09 +0530

    Deleting js files due to bugs

--- Scripts/Fullscreen.js
+++
@@ -1,9 +0,0 @@
-//Code for showing the html in fullscreen (Reference: MDN)
-const fullScreenBut = document.getElementById("fullscreen");
-fullScreenBut.addEventListener("click",function(){
-    if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
-    } else if (document.exitFullscreen) { 
-        document.exitFullscreen();
-    }
-})
